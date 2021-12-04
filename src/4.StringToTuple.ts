6c909bd3939dbd203678d44e2a0a82ac6121c032
aspire-t <zhijianwubu@gmail.com>
2021-12-04 18:11:54 +0800

    feat:4.StringToTuple

---
+++ src/4.StringToTuple.ts
@@ -0,0 +1,5 @@
+namespace StringToTuple {
+	type StringToTuple<T extends string, TT extends any[] = []> = T extends '' ? TT : T extends `${infer L}${infer R}` ? StringToTuple<R, [...TT, L]> : never
+	type A = StringToTuple<'BFE.dev'> // ['B', 'F', 'E', '.', 'd', 'e','v']
+	type B = StringToTuple<''> // []
+}
