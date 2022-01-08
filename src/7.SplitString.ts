1eeafc3a05771f161d05ccfbd224d1411660588c
aspire-t <zhijianwubu@gmail.com>
2022-01-08 23:35:27 +0800

    feat:7.splitstring

---
+++ src/7.SplitString.ts
@@ -0,0 +1,9 @@
+export default {}
+
+type SplitString<T extends string, S extends string, A extends any[] = []> = T extends `${infer L}${S}${infer R}` ? SplitString<R, S, [...A, L]> : [...A, T]
+
+type A1 = SplitString<'handle-open-flag', '-'>        // ["handle", "open", "flag"]
+type A2 = SplitString<'open-flag', '-'>               // ["open", "flag"]
+type A3 = SplitString<'handle.open.flag', '.'>        // ["handle", "open", "flag"]
+type A4 = SplitString<'open.flag', '.'>               // ["open", "flag"]
+type A5 = SplitString<'open.flag', '-'>               // ["open.flag"]
