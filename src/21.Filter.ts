28f0264d37a5b9b6f8f90d8f22354650bebfd2f8
aspire-t <zhijianwubu@gmail.com>
2022-02-01 23:33:43 +0800

    feat:23.equral

--- src/21.Filter.ts
+++ src/21.Filter.ts
@@ -1,6 +1,16 @@
 export default {}
+//保留元组类型T中的A类型
 
 type Filter<T, M, prev extends M[] = []> = T extends [infer L, ...infer R] ? [L] extends [M] ? Filter<R, M, [...prev, L]> : Filter<R, M, [...prev]> : prev
+
+/**
+ *  为什么是 [L] extends [M] 而不是 L extends M, 因为前者不分布，后者会分布
+ */
+
+/**
+分布式有条件类型
+如果有条件类型里待检查的类型是naked type parameter，那么它也被称为“分布式有条件类型”。 分布式有条件类型在实例化时会自动分发成联合类型。 例如，实例化T extends U ? X : Y，T的类型为A | B | C，会被解析为(A extends U ? X : Y) | (B extends U ? X : Y) | (C extends U ? X : Y)。
+*/
 
 type A = Filter<[1, 'BFE', 2, true, 'dev'], number> // [1, 2]
 type B = Filter<[1, 'BFE', 2, true, 'dev'], string> // ['BFE', 'dev']
---
+++ src/22.FindIndex.ts
@@ -0,0 +1,15 @@
+export default {}
+// 找出E类型在元组类型T中的下标
+type Equal<T, K> = [T] extends [K] ? [K] extends [T] ? (keyof T extends keyof K ? keyof K extends keyof T ? true : false : false) : false : false
+
+type FindIndex<T extends any[], K> = T extends [...infer left, infer last] ? Equal<K, last> extends true ? left["length"] : FindIndex<left, K> : never
+
+type index1 = FindIndex<[1, string, 2, 3, 5], string>
+type index2 = FindIndex<[1, 2, string, 3, 5], string>
+type index3 = FindIndex<[1, 2, 3, string, 5], string>
+type index4 = FindIndex<[1, 2, 3, 5, string], string>
+
+type A = [any, never, 1, '2', true]
+
+type B = FindIndex<A, 1> // 2
+type C = FindIndex<A, 3> // never
