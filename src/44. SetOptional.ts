2a889fcf297cf2f86035035f810df3a45386befe
aspire-t <zhijianwubu@gmail.com>
2022-02-13 15:42:48 +0800

    feat: setOption 注解

--- src/44. SetOptional.ts
+++ src/44. SetOptional.ts
@@ -1,5 +1,18 @@
 
-type SetOptional<T, K extends keyof T> =  Partial<Pick<T, K>> & Omit<T, K>
+export default { }
+
+/**
+ * Omit 为反选的意思，
+ * 以上面测试用例讲解
+ * 1.首先 Omit<T,K> => Omit<Foo, 'a'| 'b'> => { c: boolean }
+ * Partial<Pick<T, K>> 这里是嵌套，我们先看看 Pick<T, K>
+ * 2.Pick<T, K> => Pick<Foo, 'a' | 'b'> => { a: number, b?: string } =>
+ * 3.Partial<{ a: number, b?: string }> 所有变成可选 => { a?: number, b?: string }
+ * 4.最后我们得到：{ c: boolean } & { a?: number, b?: string }
+ * 5.合并得到之后: { a?: number, b?: string , c: boolean} 
+ */
+
+type SetOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
 
 type Foo = {
 	a: number;
