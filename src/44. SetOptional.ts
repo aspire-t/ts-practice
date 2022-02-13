8159fdd34cf5a2d2461f3758c5f0ad838af5c64c
aspire-t <zhijianwubu@gmail.com>
2022-02-13 15:38:39 +0800

    feat: setOption

---
+++ src/44. SetOptional.ts
@@ -0,0 +1,17 @@
+
+type SetOptional<T, K extends keyof T> =  Partial<Pick<T, K>> & Omit<T, K>
+
+type Foo = {
+	a: number;
+	b?: string;
+	c: boolean;
+}
+
+// 测试用例
+type SomeOptional = SetOptional<Foo, 'a' | 'b'>;
+
+// type SomeOptional = {
+// 	a?: number; // 该属性已变成可选的
+// 	b?: string; // 保持不变
+// 	c: boolean; 
+// }
