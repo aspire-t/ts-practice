f7f0a8b70e189467e1786f48c2670076d9802ad7
aspire-t <zhijianwubu@gmail.com>
2022-02-16 23:53:45 +0800

    feat: 50. EmptyObject

---
+++ src/50.EmptyObject.ts
@@ -0,0 +1,13 @@
+export default {}
+/**
+EmptyObject类型中[K in keyof any] 等同于[K in string | number | symbol]，将所有对象属性对应类型设置为never。
+ */
+type EmptyObject = {
+	[K in keyof any] : never
+}
+
+// 测试用例
+const shouldPass: EmptyObject = {} // 可以正常赋值
+const shouldFail: EmptyObject = { // 将出现编译错误
+	prop: "TS"
+}
