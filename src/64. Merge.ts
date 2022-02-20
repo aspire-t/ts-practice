8e7568bed17a2df75ca0f57446f72056361bf258
aspire-t <zhijianwubu@gmail.com>
2022-02-20 21:53:07 +0800

    feat: merge 使用omit

--- src/64. Merge.ts
+++ src/64. Merge.ts
@@ -4,6 +4,9 @@ type Merge<FirstType, SecondType> = {
 	[T in keyof FirstType & SecondType]: T extends keyof FirstType ? FirstType[T] : T extends keyof SecondType ? SecondType[T] : never
 }
 
+// 内置Omit
+// type Merge<FirstType, SecondType> = Omit<FirstType, keyof SecondType> & SecondType;
+
 type Foo = {
 	a: number
 	b: string
