bf4ed7ae207d13eb205b6b2c211cc101b55595bb
aspire-t <zhijianwubu@gmail.com>
2022-02-21 23:24:08 +0800

    feat: removeIndexSingnature

--- src/66. RemoveIndexSignature.ts
+++ src/66. RemoveIndexSignature.ts
@@ -6,7 +6,7 @@ export default {}
  * 
 1、遍历T，利用as断言实现对K的判断过滤；
 
-2、当前的key如果满足string | number直接返回never过滤当前属性；
+2、当前的key如果满足string | number | symbol 直接返回never过滤当前属性；
 
 3、否则拿当前K，当前K值类型为T[K]。
  */
@@ -16,12 +16,15 @@ type RemoveIndexSignature<T> = {
 	? never
 	: number extends K
 	? never
+	: symbol extends K
+	? never
 	: K]: T[K]
 }
 
 interface Foo {
 	[key: string]: any
 	[key: number]: any
+	[key: symbol]: any
 	bar(): void
 }
 
