f8ba7d870872d31165295153571f6da01485cacd
aspire-t <zhijianwubu@gmail.com>
2022-03-03 22:27:04 +0800

    feat: First

--- src/easy/First.ts
+++ src/easy/First.ts
@@ -1,10 +1,13 @@
 export default {}
 
 // 实现一个通用First<T>，它接受一个数组T并返回它的第一个元素的类型。
-type First<T> = T
+
+type First<T> = T extends [infer left, ...infer right] ? left : never
+
+// type First<T extends any[] = []> = T extends [] ? never : T[0]
 
 type arr1 = ['a', 'b', 'c']
 type arr2 = [3, 2, 1]
 
 type head1 = First<arr1> // expected to be 'a'
-type head2 = First<arr2> // expected to be 3`
+type head2 = First<arr2> // expected to be 3
