3861b6f265a9ac7ca25ef5c579ba04791946be54
aspire-t <zhijianwubu@gmail.com>
2022-03-05 22:18:28 +0800

    feat: myReturnType

--- src/medium/MyReturnType.ts
+++ src/medium/MyReturnType.ts
@@ -2,12 +2,12 @@ export default {}
 
 // 不使用 ReturnType 实现 TypeScript 的 ReturnType<T> 泛型。
 
-type MyReturnType<T extends (...args: unknown[]) => unknown> =
-	T extends (...args: any[]) => infer ReturnType
-	? ReturnType
-	: never
+// type MyReturnType<T extends (...args: unknown[]) => unknown> =
+// 	T extends (...args: any[]) => infer ReturnType
+// 	? ReturnType
+// 	: never
 
-// type MyReturnType<T> = T extends (...args: any[]) => infer R ? R : never;
+type MyReturnType<T> = T extends (...args: any[]) => infer R ? R : never;
 
 const fn = (v: boolean) => {
 	if (v)
