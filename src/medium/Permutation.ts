1cb79af0a8c67a9e916bf34c05cb70511afedb9b
aspire-t <zhijianwubu@gmail.com>
2022-03-18 22:26:58 +0800

    feat: Permutation

--- src/medium/Permutation.ts
+++ src/medium/Permutation.ts
@@ -1,5 +1,7 @@
 export default {}
 
+// When T and U are a single string, this Exclude<U, T> be never, then [T] extends [never] is true, return [].
+// Otherwise T should be a subset of U, eg T equals 'A' and U is equal to 'A' | 'B' | 'C'
 type Permutation<T, U = T> = [T] extends [never] ? [] : T extends U ? [T, ...Permutation<Exclude<U, T>>] : []
 
 type perm = Permutation<'A' | 'B' | 'C'> // ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']
