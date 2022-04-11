283c714230b9718be850d4f3522f0964c3f2c161
aspire-t <zhijianwubu@gmail.com>
2022-04-11 19:36:56 +0800

    feat: Zip

--- src/medium/Zip.ts
+++ src/medium/Zip.ts
@@ -1,4 +1,9 @@
 export default {}
 
+type Zip<T extends any[], U extends any[], R extends any[] = []> = T extends [infer TFirst, ...(infer TRest)]
+	? U extends [infer UFirst, ...(infer URest)]
+	? Zip<TRest, URest, [...R, [TFirst, UFirst]]>
+	: R
+	: R
+
 type exp = Zip<[1, 2], [true, false]> // expected to be [[1, true], [2, false]]
-t
